356e0553345c33a732aaac08df348c276086f243
Giner-Jv-git <jginer@filamer.edu.ph>
2025-06-03 21:33:21 +0800

    added view table

--- emp_app/urls.py
+++ emp_app/urls.py
@@ -3,5 +3,10 @@ from . import views
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    # View Table URLs
+    path('employees/', views.EmployeeTableView.as_view(), name='view_table'),
+    
+    # If you need the HR view-only version
+    
 
 ]
--- emp_app/views.py
+++ emp_app/views.py
@@ -1,8 +1,35 @@
-from django.contrib.auth.views import LoginView
-from django.shortcuts import redirect
-from django.urls import reverse
+from django.views.generic import ListView
+from django.db.models import Q
+from .models import EmployeeData
 
+class EmployeeTableView(ListView):
+    model = EmployeeData
+    template_name = 'emp_app/admin/view_table.html'
+    context_object_name = 'employees'
+    paginate_by = 10
 
-# Create your views here.
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('search', '')
+        status_filter = self.request.GET.get('status', '')
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(employee_id__icontains=search_query) |
+                Q(phone_number__icontains=search_query) |
+                Q(address__icontains=search_query)
+            )
+
+        if status_filter:
+            queryset = queryset.filter(status=status_filter)
+
+        return queryset.order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_query'] = self.request.GET.get('search', '')
+        context['status_filter'] = self.request.GET.get('status', '')
+        return context
 
 
